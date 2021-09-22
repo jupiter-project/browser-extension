bdb474eadf382824b9a20770ec92e373968339ae
greatdolphinls <thmsshanahan0815@gmail.com>
2021-09-22 19:51:41 +0000

    [feat] upgrate to manifest v3

--- public/static/background.js
+++ public/static/background.js
@@ -1,5 +1,5 @@
 // Called when the user clicks on the browser action
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
 	// Send a message to the active tab
 	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 		var activeTab = tabs[0];
